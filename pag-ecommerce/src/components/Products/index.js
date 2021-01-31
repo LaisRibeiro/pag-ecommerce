31c95e137a3d5804b62afe87b3d77b56c71d37b5
LaisRibeiro <riibeirolais@gmail.com>
2021-01-31 00:12:41 -0300

    feat: created component product

---
+++ pag-ecommerce/src/components/Products/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Products(){
+
+    return(
+        <div className='Grid'>
+            <img className='img' src="https://picsum.photos/200/300?random=3"/>
+            <img className='img' src="https://picsum.photos/200/300?random=3"/>
+            <img className='img' src="https://picsum.photos/200/300?random=3"/>
+            <img className='img' src="https://picsum.photos/200/300?random=3"/>
+            <img className='img' src="https://picsum.photos/200/300?random=3"/>
+            <img className='img' src="https://picsum.photos/200/300?random=3"/>
+        </div>
+    );
+}
--- pag-ecommerce/src/views/Home/index.js
+++ pag-ecommerce/src/views/Home/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import './styles.css';
+
 import Header from '../../components/Header';
 import Banner from '../../components/Banner';
+import Products from '../../components/Products';
 
 export default function Home(){
 
     return (
-		  <div>
-        <Header/>
-        <Banner/>
-      </div> 
+      <div>
+          <Header/>
+          <Banner/>
+          <Products/>
+      </div>
     );
 }
