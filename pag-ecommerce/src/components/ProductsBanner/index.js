7124cffd3c67d3c5e1a88967c68ab6024d50bada
LaisRibeiro <riibeirolais@gmail.com>
2021-01-31 22:42:56 -0300

    feat: create ProductsBanner

---
+++ pag-ecommerce/src/components/ProductsBanner/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function ProductsBanner(){
+
+    return(
+        <div>
+            <div className='faixa'/>
+            <div className='gridFaixa'>
+                <div className='gridTitle'>
+                    <h5 className='h1'>Lorem ipsum lorem dolor</h5>
+                    <h2 className='h1'>Lorem ipsum dolor sit amet</h2>
+                    <h5 className='h1'>Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna
+                        aliqua. Metus vulputate eu scelerisque
+                        felis imperdiet proin fermentum. Praesent
+                        semper feugiat nibh sed pulvinar proin
+                        gravida.</h5>
+                </div>
+            </div>
+        </div>
+    );
+}
--- pag-ecommerce/src/views/Home/index.js
+++ pag-ecommerce/src/views/Home/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import './styles.css';
 
 import Header from '../../components/Header';
 import Banner from '../../components/Banner';
 import Products from '../../components/Products';
+import ProductsBanner from '../../components/ProductsBanner';
 
 export default function Home(){
 
     return (
       <div>
-          <Header/>
-          <Banner/>
+          {/* <Header/>
+          <Banner/> */}
           <Products/>
+          <ProductsBanner/>
       </div>
     );
 }
