9dfa37ebfdd9cbdf829c83c7eacb9ad78e084da7
LaisRibeiro <riibeirolais@gmail.com>
2021-02-01 22:05:35 -0300

    fix: ajust layout

--- pag-ecommerce/src/routes/index.js
+++ pag-ecommerce/src/routes/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from '../views/Home';
-import BannerHome from '../views/BannerHome';
 
 export default function Routes() {
     return (
       <Router>
         <Switch>
             <Route exact path="/" component={Home}/>
-            <Router path="/banner" component={BannerHome}/>
         </Switch>
     </Router>
   );
--- pag-ecommerce/src/views/Home/index.js
+++ pag-ecommerce/src/views/Home/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './styles.css';
-
 import Header from '../../components/Header';
 import Banner from '../../components/Banner';
 import Products from '../../components/Products';
